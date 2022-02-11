174bb89b32c011dcc38fc7e26ef0136a51afc282
Aaron Calahorra <97182476+aaron-calahorra@users.noreply.github.com>
2022-02-10 18:17:42 -0700

    Update

--- scripts/task6.js
+++ scripts/task6.js
@@ -1,3 +1,4 @@
+const body = document.querySelector("body");
 const css = document.querySelector("h3");
 const color1 = document.querySelector(".color1");
 const color2 = document.querySelector(".color2");
